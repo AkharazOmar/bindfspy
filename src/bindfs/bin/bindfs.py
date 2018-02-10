a14920d2d95f9686d31e67dcb05f919990246895
Omar AKHARAZ <omar.akharaz@gmail.com>
2018-02-11 00:11:16 +0100

    check the argument with argparse.type

--- src/bindfs/bin/bindfs.py
+++ src/bindfs/bin/bindfs.py
@@ -3,11 +3,10 @@ import sys
 import argparse
 import logging
 
-
+from pathlib import Path
 from contextlib import contextmanager
 
 from fuse import FUSE
-from os.path import realpath
 
 from bindfs.bindfs import BindFs
 
@@ -25,25 +24,55 @@ def run_fuse(src, dst, *args, **kwargs):
         FUSE(BindFs(src_fd), dst, *args, foreground=True, **kwargs)
 
 
+def directory(dir_path, raise_error: bool=True):
+    real_path = Path(dir_path).resolve()
+    if not real_path.is_dir():
+        if raise_error:
+            raise argparse.ArgumentParser("{} must exist directory.".format(real_path.as_posix()))
+        else:
+            return None
+    return real_path.as_posix()
+
+
 def main(*args, **kwargs):
     parse = argparse.ArgumentParser(
         description="bindfspy: FIXME"
     )
 
-    parse.add_argument("-s", "--src", type=str, required=True)
-    parse.add_argument("-d", "--dst", type=str, required=True)
+    parse.add_argument(
+        "-s",
+        "--src",
+        type=directory,
+        required=True,
+        help="The source directory to bind.",
+    )
+    parse.add_argument(
+        "-d",
+        "--dst",
+        type=directory,
+        required=True,
+        help="The mount point (destination directory).",
+    )
     # foreground_parse = parse.add_mutually_exclusive_group(required=False)
-    parse.add_argument("-f", "--foreground", dest="foreground", action="store_true", default=False)
+    parse.add_argument(
+        "-f",
+        "--foreground",
+        dest="foreground",
+        action="store_true",
+        default=False,
+        help="Launch process in foreground more. Default=False"
+    )
     args_parse = parse.parse_args()
 
-    src = realpath(args_parse.src)
-    dst = realpath(args_parse.dst)
+    src = args_parse.src
+    dst = args_parse.dst
 
     if src == dst:
         kwargs["nonempty"] = True
 
     if (not args_parse.foreground and os.fork() == 0) or args_parse.foreground:
         run_fuse(src, dst, *args, **kwargs)
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
